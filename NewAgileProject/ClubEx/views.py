ad32c5c4a3ec0bbe9a5f4494a3946288939c5717
Charlie Maru <charliemaru@gmail.com>
2020-09-01 21:35:02 +1200

    Update views.py
    added functions for all pages to views.py file

--- NewAgileProject/ClubEx/views.py
+++ NewAgileProject/ClubEx/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+# from .models import Video
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'gymapp/index.html', {'title': 'Welcome to Gym App'})
+
+
+def classes(request):
+    return render(request, 'gymapp/classes.html', {'title': 'Classes'})
+
+
+def classes_boxing(request):
+    return render(request, 'gymapp/boxing.html', {'title': 'Boxing'})
+
+
+def classes_pilates(request):
+    return render(request, 'gymapp/pilates.html', {'title': 'Pilates'})
+
+
+def classes_aerobics(request):
+    return render(request, 'gymapp/aerobics.html', {'title': 'Aerobics'})
+
+
+def classes_yoga(request):
+    return render(request, 'gymapp/yoga.html', {'title': 'Yoga'})
+
+
+def classes_spinning(request):
+    return render(request, 'gymapp/spinning.html', {'title': 'Spinning'})
+
+
+def classes_tai_chi(request):
+    return render(request, 'gymapp/taichi.html', {'title': 'Tai Chi'})
+
+
+def plans(request):
+    return render(request, 'gymapp/plans.html',  {'title': 'Plans'})
+
+
+def contact(request):
+    return render(request, 'gymapp/contact.html',  {'title': 'Contacts'})
+
+
+def about(request):
+    return render(request, 'about.html', {'title': 'About Us'})
+
+'''
+def video(request):
+    obj = Video.objects.all()
+    return render(request, 'videos.html', {'obj': obj})
+'''
