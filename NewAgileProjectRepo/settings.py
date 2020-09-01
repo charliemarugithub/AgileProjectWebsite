e2d7928bdfe14c1f0eb2a5492a15b8829ce417bf
Charlie Maru <charliemaru@gmail.com>
2020-09-02 11:34:31 +1200

    added Registration page
    added Registration page  and users app to projecgt

--- NewAgileProjectRepo/settings.py
+++ NewAgileProjectRepo/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'ClubEx.apps.ClubexConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
