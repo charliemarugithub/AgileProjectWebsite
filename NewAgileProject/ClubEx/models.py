9d58cc255b6d7445d78a7f2978b5aea50c2b7005
Charlie Maru <charliemaru@gmail.com>
2020-09-01 22:40:26 +1200

    reverted back and started from views.py
    Removing all references to embed-video that I can see.

--- NewAgileProject/ClubEx/models.py
+++ NewAgileProject/ClubEx/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-from embed_video.fields import EmbedVideoField
 
 
 class Video(models.Model):
     category = models.CharField(max_length=200)
     video_name = models.CharField(max_length=200)
     content = models.CharField(max_length=500)
-    video = EmbedVideoField()  # same like models.URLField()
     stats = models.CharField(max_length=20)
     slug = models.SlugField(unique=True)
 
