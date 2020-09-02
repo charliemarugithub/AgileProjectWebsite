ec4a807ac34b0360107addcb9f690b4710879a16
Charlie Maru <charliemaru@gmail.com>
2020-09-02 17:12:06 +1200

    added registration form

--- ClubEx/models.py
+++ ClubEx/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
+
 class Video(models.Model):
     category = models.CharField(max_length=200)
     video_name = models.CharField(max_length=200)
     content = models.CharField(max_length=200)
     stats = models.CharField(max_length=100)
-    
- 
