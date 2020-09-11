9059359166e39703649a3ef7ee759b1e2c9a03d5
JamesMitchell92020474 <jamesmitchelldesign1@gmail.com>
2020-09-11 16:12:19 +1200

    Home page 'Done'

--- ClubEx/models.py
+++ ClubEx/models.py
@@ -5,9 +5,11 @@ from embed_video.fields import EmbedVideoField
 class Video(models.Model):
     category = models.CharField(max_length=200)
     video_name = models.CharField(max_length=200)
-    video = models.URLField()
+    video = EmbedVideoField()
     content = models.TextField()
 
     def __str__(self):
         return self.video_name
+
+
 
